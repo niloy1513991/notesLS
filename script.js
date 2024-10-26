295cb2bd8d9e2593bb24bf9caaa7bc2b949f481f
niloykm <niloy@Nioys-MacBook-Pro.local>
2024-10-26 12:26:20 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const notesContainer = document.getElementById("notesContainer");
+const createBtn = document.getElementById("btn");
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes") || '';
+    addEventListenersToNotes();
+}
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+function addEventListenersToNotes() {
+    const notes = document.querySelectorAll(".input-box");
+    notes.forEach(note => {
+        note.addEventListener("keyup", updateStorage);
+    });
+}
+
+createBtn.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.innerHTML = "&#8203;";
+    inputBox.className = "input-box";
+    img.className = "img";
+    inputBox.setAttribute("contenteditable", "true");
+    inputBox.setAttribute("data-placeholder", "Start typing here...");
+    img.src = "/images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+    addEventListenersToNotes();
+    updateStorage();
+});
+
+notesContainer.addEventListener("click", function(e) {
+    if (e.target.tagName.toLowerCase() === "img") {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+});
+
+showNotes();
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './index.html',
+    './script.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
